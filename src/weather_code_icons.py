2603962efa0141e24626741e843f191613505f13
Chukwuemeka Aligbe <cealigbe@gmail.com>
2026-08-04 03:29:30 -0400

    Updated weather codes

--- src/weather_code_icons.py
+++ src/weather_code_icons.py
@@ -6,6 +6,7 @@ weather_code_icons = {
     1006: 119,  # Cloudy
     1009: 122,  # Overcast
     1030: 143,  # Mist
+    1036: 143,  # Smoky haze
     1063: 176,  # Patchy rain possible
     1066: 179,  # Patchy snow possible
     1069: 182,  # Patchy sleet possible
